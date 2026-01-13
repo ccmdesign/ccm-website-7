e0428dc553864ff836d181777c5be69089b10c58
Claudio Mendonca <claudioccm@gmail.com>
2026-01-13 12:41:39 -0800

    mailchimp form

--- components/organisms/ccmCtaSection.vue
+++ components/organisms/ccmCtaSection.vue
@@ -1,8 +1,24 @@
 <template>
   <section class="ccm-cta-section">
-    <form class="form | stack">
-      <input type="email" label="Email" placeholder="Email" />
-      <div class="button-container"><input type="submit" label="Subscribe" value="Subscribe" class="button" variant="primary" data-size="s"/></div>
+    <form
+      action="https://claudiomendonca.us2.list-manage.com/subscribe/post?u=468ffb5e21f0082332ecdd5f3&amp;id=eaa305764b&amp;f_id=0074d8e3f0"
+      method="post"
+      target="_blank"
+      class="form | stack"
+    >
+      <input
+        type="email"
+        name="EMAIL"
+        placeholder="Email"
+        required
+      />
+      <!-- Honeypot field to prevent bot signups -->
+      <div aria-hidden="true" style="position: absolute; left: -5000px;">
+        <input type="text" name="b_468ffb5e21f0082332ecdd5f3_eaa305764b" tabindex="-1" value="">
+      </div>
+      <div class="button-container">
+        <input type="submit" name="subscribe" value="Subscribe" class="button" variant="primary" data-size="s"/>
+      </div>
     </form>
   </section>
 </template>
